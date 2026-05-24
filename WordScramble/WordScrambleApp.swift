ac4f67423623b33746e336ecc1f4c3329dbb9bfa
Yuling <yuling.sg@gmail.com>
2026-05-24 22:19:50 +0900

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chen Yuling on 2026/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
